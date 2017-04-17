670ea4c2514c71b050c92a8fa4a3c773e18e305b
Radi A <radi.a@live.com.au>
2017-04-17 15:29:25 +0300

    Added a generic EF repository

---
+++ StarterSolution.Data/Repositories/EntityFrameworkRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace StarterSolution.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    public class EntityFrameworkRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        public EntityFrameworkRepository(DbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> match)
+        {
+            return await Context.Set<TEntity>().Where(match).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            return await Context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
