d3f30569d70bd9efed65fb738ca2894664df9bee
Radi A <radi.a@live.com.au>
2017-04-17 15:30:04 +0300

    Added a basic configuration sclass

---
+++ StarterSolution.Data/EntityConfigurations/CarConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace StarterSolution.Data.EntityConfigurations
+{
+    using StarterSolution.Data.Models;
+    using System.Data.Entity.ModelConfiguration;
+
+    public class CarConfiguration : EntityTypeConfiguration<Car>
+    {
+        public CarConfiguration()
+        {
+
+        }
+    }
+}
