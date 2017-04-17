f0c15dd2a02a04782c722cbede8d3d21654710a8
Radi A <radi.a@live.com.au>
2017-04-17 15:30:12 +0300

    Unit of Work implementation

---
+++ StarterSolution.Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿namespace StarterSolution.Data
+{
+    using StarterSolution.Data.Interfaces;
+    using System.Threading.Tasks;
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CarApplicationDbContext _context;
+
+        public IPartsRepository Parts { get; private set; }
+
+        public ICarsRepository Cars { get; private set; }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
