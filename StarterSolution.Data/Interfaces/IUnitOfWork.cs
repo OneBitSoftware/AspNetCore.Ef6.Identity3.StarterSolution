d137307cd5b0953b03d41c36bc472b9665d1a370
Radi A <radi.a@live.com.au>
2017-04-17 15:29:13 +0300

    Added a Unit Of Work interface

---
+++ StarterSolution.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace StarterSolution.Data.Interfaces
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IUnitOfWork : IDisposable
+    {
+        IPartsRepository Parts { get; }
+        ICarsRepository Cars { get; }
+        Task<int> CompleteAsync();
+    }
+}
