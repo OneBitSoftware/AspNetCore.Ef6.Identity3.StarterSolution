6c5c8c0a768b069b6317a6a39a16e194447b6ebb
Radi A <radi.a@live.com.au>
2017-04-17 15:29:37 +0300

    Added specific repositories

---
+++ StarterSolution.Data/Repositories/CarsRepository.cs
@@ -0,0 +1,27 @@
+﻿using StarterSolution.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterSolution.Data
+{
+    public class CarsRepository : EntityFrameworkRepository<Car>, ICarsRepository
+    {
+        public CarApplicationDbContext CarsContext
+        {
+            get { return Context as CarApplicationDbContext;}
+        }
+
+        public CarsRepository(CarApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Car> GetTopSellingCars(int count)
+        {
+            return CarsContext.Cars.Take(10).ToList();
+        }
+    }
+}
---
+++ StarterSolution.Data/Repositories/PartsRepository.cs
@@ -0,0 +1,28 @@
+﻿using StarterSolution.Data.Interfaces;
+using StarterSolution.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterSolution.Data.Repositories
+{
+    public class PartsRepository : EntityFrameworkRepository<Part>, IPartsRepository
+    {
+        public CarApplicationDbContext CarsContext
+        {
+            get { return Context as CarApplicationDbContext; }
+        }
+
+        public PartsRepository(CarApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Part> GetPartsForCar(Car car)
+        {
+            return CarsContext.Parts.Take(10).ToList();
+        }
+    }
+}
