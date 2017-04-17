1ca637c148e7972cd2fef405c9473faff2d670fb
Radi A <radi.a@live.com.au>
2017-04-17 15:28:55 +0300

    Added simple models

---
+++ StarterSolution.Data/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace StarterSolution.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string SensitiveData { get; set; }
+    }
+}
---
+++ StarterSolution.Data/Models/Part.cs
@@ -0,0 +1,8 @@
+﻿namespace StarterSolution.Data.Models
+{
+    public class Part
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
