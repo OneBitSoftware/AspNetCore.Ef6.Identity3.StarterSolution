2d1451a1fe819be62e8ed9f00122d5b390b52177
Radi A <radi.a@live.com.au>
2017-04-17 15:33:12 +0300

    Simple cars view model, factory and view

---
+++ StarterSolution.Web/Factories/CarModelFactory.cs
@@ -0,0 +1,35 @@
+﻿using StarterSolution.Data.Models;
+using StarterSolution.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterSolution.Web.Factories
+{
+    public static class CarModelFactory
+    {
+        public static CarViewModel CreateCarViewModel(Car car)
+        {
+            var carViewModel = new CarViewModel();
+
+            carViewModel.Make = car.Make;
+            carViewModel.Model = car.Model;
+            carViewModel.Year = car.Year;
+
+            return carViewModel;
+        }
+
+        public static IEnumerable<CarViewModel> CreateCarViewModels(IEnumerable<Car> cars)
+        {
+            var carViewModels = new List<CarViewModel>();
+
+            foreach (var car in cars)
+            {
+                carViewModels.Add(CreateCarViewModel(car));
+            }
+
+            return carViewModels;
+        }
+    }
+}
---
+++ StarterSolution.Web/Models/CarViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StarterSolution.Web.Models
+{
+    public class CarViewModel
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
