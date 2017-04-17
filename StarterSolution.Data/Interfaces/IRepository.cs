d03ce875a404853f104671c2a81b1885559687a2
Radi A <radi.a@live.com.au>
2017-04-17 15:28:43 +0300

    Added the base IRepository interface

---
+++ StarterSolution.Data/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace StarterSolution.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    public interface IRepository<TEntity> where TEntity: class
+    {
+        Task<TEntity> GetAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> match);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+        Task RemoveRangeAsync(IEnumerable<TEntity> entity);
+    }
+}
