130898098bc0cd8d0674a1c74c498d063b552743
Radi A <radi.a@live.com.au>
2017-04-17 15:32:55 +0300

    Created a CarService and interface

---
+++ StarterSolution.Web/Services/CarService.cs
@@ -0,0 +1,32 @@
+﻿namespace StarterSolution.Web.Services
+{
+    using StarterSolution.Data;
+    using StarterSolution.Data.Models;
+    using StarterSolution.Web.Factories;
+    using StarterSolution.Web.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class CarService : ICarService
+    {
+        private ICarsRepository _carsRepository;
+
+        public CarService(ICarsRepository carsRepository)
+        {
+            _carsRepository = carsRepository ?? throw new ArgumentNullException(nameof(carsRepository));
+        }
+
+        public async Task<CarViewModel> CreateCarAsync(Car car)
+        {
+            var createdCar = await _carsRepository.AddAsync(car);
+            return CarModelFactory.CreateCarViewModel(createdCar);
+        }
+
+        public async Task<IEnumerable<CarViewModel>> GetCarsAsync()
+        {
+            var cars = await _carsRepository.GetAllAsync();
+            return CarModelFactory.CreateCarViewModels(cars);
+        }
+    }
+}
---
+++ StarterSolution.Web/Services/ICarService.cs
@@ -0,0 +1,13 @@
+﻿namespace StarterSolution.Web.Services
+{
+    using StarterSolution.Data.Models;
+    using StarterSolution.Web.Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface ICarService
+    {
+        Task<CarViewModel> CreateCarAsync(Car car);
+        Task<IEnumerable<CarViewModel>> GetCarsAsync();
+    }
+}
